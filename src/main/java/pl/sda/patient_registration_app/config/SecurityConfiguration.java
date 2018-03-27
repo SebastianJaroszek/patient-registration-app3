248ea28643a9b26992c91e07ddad8e42d985c307
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-03-27 18:57:47 +0200

    Import BCrypt library.

--- src/main/java/pl/sda/patient_registration_app/config/SecurityConfiguration.java
+++ src/main/java/pl/sda/patient_registration_app/config/SecurityConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import pl.sda.patient_registration_app.handler.SuccessLoginHandler;
